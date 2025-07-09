de2b438e3edc99efe448af928e2d79eabd3ced03
Henry Agnew <Miniland1333@users.noreply.github.com>
2025-07-09 12:03:36 -0700

    Added downloads

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -3,8 +3,27 @@
 import React from "react";
 import content from "@/content/downloads.json";
 
+interface DownloadEntry{
+    version:string;
+    date:string;
+    type?: string;
+    patch_notes?: string;
+    download_link?: string;
+}
+
 export default function Download() {
-    const data = content.versions;
+    const content_data: DownloadEntry[] = content.versions;
+
+    let data: DownloadEntry[] = [];
+    for (const content_item of content_data){
+        let item = {...content_item}
+        item.type ??= "Source ZIP"
+        item.patch_notes ??= `https://github.com/paesanilab/MBX-dev/releases/tag/v${item.version}`
+        item.download_link ??= `https://github.com/paesanilab/MBX-dev/releases/tag/v${item.version}`
+        data.push(item)
+    }
+
+
 
     return (
         <div className="bg-[#030625] text-white min-h-screen h-full rounded-[40px] m-[20px] md:m-[70px]">
@@ -20,8 +39,8 @@ export default function Download() {
                     <div className="flex justify-between bg-[#0C1E8C] items-center rounded-sm text-md font-bold h-[42px]">
                         <span className="w-1/5 flex justify-center">Version</span>
                         <span className="w-1/5 flex justify-center">Upload Date</span>
-                        <span className="w-1/5 flex justify-center">Size</span>
                         <span className="w-1/5 flex justify-center">Type Download</span>
+                        <span className="w-1/5 flex justify-center">Patch Notes</span>
                         <span className="w-1/5 flex justify-center">Download</span>
                     </div>
 
@@ -33,13 +52,20 @@ export default function Download() {
                         >
                             <span className="w-1/5 flex justify-center">{item.version}</span>
                             <span className="w-1/5 flex justify-center">{item.date}</span>
-                            <span className="w-1/5 flex justify-center">{item.size}</span>
                             <span className="w-1/5 flex justify-center text-[#4057E6]">
                                 {item.type}
                             </span>
                             <span className="w-1/5 flex justify-center">
                                 <a
-                                    href={item.link}
+                                    href={item.patch_notes}
+                                    className="bg-[#60A7FF] text-white font-bold px-4 py-1 rounded-3xl hover:bg-[#508fd4] transition text-xs md:px-6 md:py-2 md:text-sm"
+                                >
+                                    Patch Notes
+                                </a>
+                            </span>
+                            <span className="w-1/5 flex justify-center">
+                                <a
+                                    href={item.download_link}
                                     download
                                     className="bg-[#60A7FF] text-white font-bold px-4 py-1 rounded-3xl hover:bg-[#508fd4] transition text-xs md:px-6 md:py-2 md:text-sm"
                                 >
@@ -66,16 +92,20 @@ export default function Download() {
                                 <span>{item.date}</span>
                             </div>
                             <div className="flex justify-between mt-2">
-                                <span className="font-bold">Size:</span>
-                                <span>{item.size}</span>
-                            </div>
-                            <div className="flex justify-between mt-2">
                                 <span className="font-bold">Type Download:</span>
                                 <span>{item.type}</span>
                             </div>
                             <div className="flex justify-center mt-4">
                                 <a
-                                    href={item.link}
+                                    href={item.patch_notes}
+                                    className="bg-[#60A7FF] text-white font-bold px-6 py-2 rounded-3xl hover:bg-[#508fd4] transition text-sm"
+                                >
+                                    Patch Notes
+                                </a>
+                            </div>
+                            <div className="flex justify-center mt-4">
+                                <a
+                                    href={item.download_link}
                                     download
                                     className="bg-[#60A7FF] text-white font-bold px-6 py-2 rounded-3xl hover:bg-[#508fd4] transition text-sm"
                                 >
@@ -87,10 +117,10 @@ export default function Download() {
                 </div>
 
                 {/* Packages Section */}
-                <div className="flex flex-col pt-[50px]">
+                {/* <div className="flex flex-col pt-[50px]">
                     <h3 className="text-2xl font-bold pb-[30px]">Packages</h3>
                     <p className="text-sm">{content.bottom_description}</p>
-                </div>
+                </div> */}
             </div>
         </div>
     );
