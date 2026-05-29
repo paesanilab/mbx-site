cc7515789650167c43c9b67ae1d05bed9f331b27
Henry Agnew <hagnew@ucsd.edu>
2026-05-28 23:13:28 -0700

    Update build chain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: process.env.BASE_PATH ?? "/",
     plugins: [react()],
     resolve: {
         alias: {
